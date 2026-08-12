26e72d7376737b0c29dc010dc4fdd18965c4b82c
braga-gustavo1001 <braga.gustavo@outlook.com.br>
2026-08-12 01:02:18 -0300

    feat: add intl package for date formatting and update main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
-main() => runApp(ExpensesApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR');
+  runApp(ExpensesApp());
+}
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
@@ -20,69 +26,97 @@ class MyHomePage extends StatelessWidget {
       value: 150.00,
       date: DateTime.now(),
     ),
-    Transaction(id: '2', title: 'Vivo', value: 168.00, date: DateTime.now()),
+    Transaction(id: '2', title: 'Vivo', value: 168.99, date: DateTime.now()),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Despesas Pessoas')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
+      appBar: AppBar(title: Text('Despesas Pessoais')),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
+              ),
             ),
-          ),
-          Column(
-            children: _transcations.map((transaction) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        transaction.value.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
+            Column(
+              children: _transcations.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
                         ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.title,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "R\$ ${transaction.value.toStringAsFixed(2)}",
                           style: TextStyle(
-                            fontSize: 16,
                             fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
                           ),
                         ),
-                        Text(
-                          transaction.date.toString(),
-                          style: TextStyle(color: Colors.grey),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transaction.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('d MMM y').format(transaction.date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(decoration: InputDecoration(labelText: 'Título')),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Valor (R\$)'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          child: Text('Nova transação'),
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.purple,
+                          ),
                         ),
                       ],
                     ),
                   ],
                 ),
-              );
-            }).toList(),
-          ),
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
